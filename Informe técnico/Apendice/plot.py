f1364ec37bda1a8e0f29188a35fa58edd7c014b2
El Leandro <elleandro@gmail.com>
2015-02-17 22:14:08 -0300

    Modificaciones menores.

--- Informe técnico/Apendice/plot.py
+++ Informe técnico/Apendice/plot.py
@@ -1,40 +1,36 @@
 #!/usr/bin/python
 # coding: latin-1
-# La segunda línea (# coding: latin-1) es necesaria para
-# poder usar acentos y ñ. Para entender el porqué, visitar:
-# http://www.python.org/dev/peps/pep-0263/
+# La segunda línea (# coding: latin-1) es necesaria para poder usar acentos y ñ. 
 from numpy import * 
-#import pylab
 import sys
 import matplotlib.pyplot as plt 
 # Primero cargo el resultado de la simulación en un arreglo:
 filename_str = sys.argv[1]
 data = genfromtxt(filename_str, skip_header=0,unpack=True)
 
-# http://matplotlib.sourceforge.net/api/figure_api.html#module-matplotlib.figure 
-print sys.argv[1]
+print sys.argv[1] # Imprimo el nombre del archivo a graficar.
 totalPlots = len(data)
 ###########################
-# nob subplots sharing both x/y axes
+# totalPlots compartiendo los ejes x/y.
 f, eje = plt.subplots(totalPlots, sharex=True, sharey=False)
 
-#eje[0].plot(data[0], data[1])
-#eje[0].set_ylabel('a0')
-#eje[0].set_title(filename_str )
-
-# Comienzo las etiquetas del resultado a partir del 3 elemento:
+# Comienzo las etiquetas del resultado a partir del 2do. elemento:
 for i in range(1,len(eje)):
    eje[i].plot(data[0], data[i])
    eje[i].set_ylabel("s"+str(i))
 
+# Escribo la primera etiqueta:
+#eje[0].plot(data[0], data[1])
+#eje[0].set_ylabel('a0')
+#eje[0].set_title(filename_str )
+
 # Escribo la segunda etiqueta:
 #eje[1].plot(data[0], data[2])
 #eje[1].set_ylabel('cout')
+#Los nombres de las etiquetas son arbitrarios, pero el orden depende de como se hayan guardado en la simulacion.
 
-
-
-# Fine-tune figure; make subplots close to each other and hide x ticks for
-# all but bottom plot.
+# Mejoro un poco la figura; acerco los sub graficos uno a otro y escondo los ticks
+# en x para todo el grafico de abajo.
 f.subplots_adjust(hspace=0)
 plt.setp([a.get_xticklabels() for a in f.axes[:-1]], visible=False)
 
